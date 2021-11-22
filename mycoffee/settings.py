b057c8785e14007511a81df2971c357277b50506
saadaoui-salah <salahsaadaoui8@gmail.com>
2021-11-22 20:52:45 +0100

    add env variables

--- mycoffee/settings.py
+++ mycoffee/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rl4b5%wgwum_0*e#n763f0%03#kwcap5czv%qj+jv6%2kcof4_'
+SECRET_KEY = os.env.get('SECRET-KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
